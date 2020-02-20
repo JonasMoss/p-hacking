9e185bc09cbb406f92337e11aed4282835a2070a
Jonas Moss <jonas.gjertsen@gmail.com>
2020-02-20 19:20:25 +0100

    Initial Overleaf Import

---
+++ R/plot.R
@@ -0,0 +1,142 @@
+# This function is used to avoid cluttering the environment when plotting.
+.H = function(expr) rlang::new_function(alist(), substitute(expr))()
+
+### ============================================================================
+### Anderson example
+### ============================================================================
+
+.H({
+jpeg("plots/anderson2010.jpg", 
+     width = 8, height = 8, units = 'in', res = 300,
+     quality = 100)
+
+par(mar = par("mar") + c(0, 0, 0, 1))
+z = seq(0, 1000, by = 0.1)
+par(las = 1)
+plot(x = 1/anderson$vi, 
+     y = anderson$yi,
+     log = "x",
+     xlab = "Precision, log scale", ylab = "Estimated effect size",
+     pch = anderson$best*19)
+lines(x = z,
+      y = 1.96*sqrt(1/z), lty = 3)
+lines(x = z,
+      y = 1.64*sqrt(1/z), lty = 2)
+
+
+abline(h = publipha::extract_theta0(ma_anderson_best$cma), lwd = 1, col = "grey")
+abline(h = publipha::extract_theta0(ma_anderson_worst$cma), lwd = 1, col = "grey")
+ticks = c(publipha::extract_theta0(ma_anderson_best$cma),
+          publipha::extract_theta0(ma_anderson_worst$cma))
+axis(side = 4, at = ticks, labels = round(ticks, 2))
+dev.off()  
+})
+
+# Posterior for theta
+
+.H({
+jpeg("plots/anderson_posterior.jpg", 
+     width = 8, height = 8, units = 'in', res = 300,
+     quality = 100)
+
+plot(density(extract_theta0(ma_anderson_all$cma, identity), adjust = 3),
+     ylim = c(0, 16), xlim = c(-0.1, .5), lwd = 2, xlab = expression(theta[0]),
+     main = NA)
+lines(density((extract_theta0(ma_anderson_all$psma, identity)), adjust = 3),
+      lty = 3, lwd = 2)
+lines(density((extract_theta0(ma_anderson_all$phma, identity)), adjust = 3), 
+      lty = 2, lwd = 2)
+dev.off()  
+})
+
+# Comparison of posteriors.
+
+.H({
+jpeg("plots/anderson_posterior_1.jpg", 
+     width = 8, height = 8, units = 'in', res = 300,
+     quality = 100)
+
+plot(density(extract_theta0(ma_anderson_all$psma, identity), adjust = 3),
+     ylim = c(0, 15), xlim = c(-0.5, .5), lwd = 2, main = NA,
+     xlab = expression(theta[0]))
+lines(density((extract_theta0(ma_anderson_best$psma, identity)), adjust = 3),
+      lty = 3, lwd = 2)
+lines(density((extract_theta0(ma_anderson_worst$psma, identity)), adjust = 3), 
+      lty = 2, lwd = 2)
+
+dev.off()  
+})
+
+.H({
+jpeg("plots/anderson_posterior_2.jpg", 
+     width = 8, height = 8, units = 'in', res = 300,
+     quality = 100)
+
+plot(density(extract_theta0(ma_anderson_all$cma, identity), adjust = 3),
+     ylim = c(0, 15), xlim = c(-0.1, .5), lwd = 2, main = NA,
+     xlab = expression(theta[0]))
+lines(density((extract_theta0(ma_anderson_best$cma, identity)), adjust = 3),
+      lty = 3, lwd = 2)
+lines(density((extract_theta0(ma_anderson_worst$cma, identity)), adjust = 3), 
+      lty = 2, lwd = 2)
+
+dev.off()  
+})
+
+### ============================================================================
+### Cuddy example.
+### ============================================================================
+
+.H({
+   jpeg("plots/cuddy2018.jpg", 
+        width = 8, height = 8, units = 'in', res = 300,
+        quality = 100)
+   par(mar = par("mar") + c(0, 0, 0, 1))
+   z = seq(0, 1000, by = 0.1)
+   par(las = 1)
+   plot(x = 1/dat.cuddy2018$vi, 
+        y = dat.cuddy2018$yi,
+        pch = 19, log = "x", 
+        ylim = c(-0.1, 1.7),
+        xlab = "Precision, log scale", 
+        ylab = "Estimated effect size")
+   lines(x = z, y = 1.96*sqrt(1/z), lty = 3)
+   lines(x = z, y = 1.64*sqrt(1/z), lty = 2)
+   abline(h = publipha::extract_theta0(ma_cuddy2018$cma), lwd = 1, col = "grey")
+   abline(h = publipha::extract_theta0(ma_cuddy2018$psma), lwd = 1, col = "grey")
+   abline(h = publipha::extract_theta0(ma_cuddy2018$phma), lwd = 1, col = "grey")
+   
+   ticks = c(publipha::extract_theta0(ma_cuddy2018$cma),
+             publipha::extract_theta0(ma_cuddy2018$psma),
+             publipha::extract_theta0(ma_cuddy2018$phma))
+   
+   axis(side = 4, at = ticks, labels = round(ticks, 2))
+   
+   dev.off()  
+})
+
+
+# Posterior for theta
+
+.H({
+jpeg("plots/cuddy2018_posterior.jpg", 
+     width = 8, height = 8, units = 'in', res = 300,
+     quality = 100)
+index = 4
+plot(density(extract_theta(ma_cuddy2018$cma, identity, i = index), adjust = 2), 
+     xlim = c(-0.7, 1.3),
+     main = NA, sub = NA, xlab = "Effect Size", "Density",
+     lwd = 2,
+     lty = 1)
+lines(density(extract_theta(ma_cuddy2018$phma, identity, i = index), adjust = 2),
+      lwd = 2,
+      lty = 2)
+lines(density(extract_theta(ma_cuddy2018$psma, identity, i = index), adjust = 2),
+      lwd = 2, 
+      lty = 3)
+axis(side = 1, 
+     at = dat.cuddy2018$yi[index], 
+     label = round(dat.cuddy2018$yi[index], 2))
+abline(v = dat.cuddy2018$yi[index], col = "grey")
+dev.off()  
+})
---
+++ R/simulate.R
@@ -0,0 +1,43 @@
+source("functions.R")
+
+# These parameters are common to all simulations.
+
+parameters = expand.grid(n = c(5, 30, 100),
+                         theta0s = c(0, 0.2, 0.8),
+                         taus = c(0.1, 0.5))
+
+lower = 20
+upper = 80
+alpha = c(0, 0.025, 0.05, 1)
+eta_ph = c(0.6, 0.3, 0.1)
+eta_ps = c(1, 0.7, 0.1)
+
+N = 3
+
+### ============================================================================
+### Actual simulation
+### ============================================================================
+
+FUN = function(parameter, type) {
+  n = parameter[1]
+  theta0 = parameter[2]
+  tau = parameter[3]
+  
+  if(type == "pb") {
+    eta = eta_ps
+  } else {
+    eta = eta_ph
+  }
+  
+  estimates(N = N, n = n, lower = lower, upper = upper, theta0 = theta0, 
+            tau = tau, alpha = alpha, eta = eta, type = type,
+            chains = 1)
+}
+
+set.seed(313)
+results_ph = apply(X = parameters, MARGIN = 1, FUN = FUN, type = "ph")
+results_pb = apply(X = parameters, MARGIN = 1, FUN = FUN, type = "pb")
+results_cl = apply(X = parameters, MARGIN = 1, FUN = FUN, type = "cl")
+saveRDS(results_ph, "../data/results_ph.Rds")
+saveRDS(results_ps, "../data/results_ps.Rds")
+saveRDS(results_cl, "../data/results_cl.Rds")
